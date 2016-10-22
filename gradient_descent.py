884086f455caed7f84b17133f237c3b6ed4b40db
minigonche <minigonche@gmail.com>
2016-10-22 23:22:53 +0000

    Fisrst method

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,10 +1,13 @@
 #Script for experiments with gradient descent
 
 #-------- Script Imports ------------
+#To make division easier
+from __future__ import division
 #For matrix and vector structures
 import numpy as np
 #For math operations
 import math as math
+
 
 
 #Runs the gradient descent with the given parameters
@@ -51,33 +54,77 @@ def run_gradient_descent(dim, fun, gradient, alpha, B_matrix, eps):
         grad = gradient(x_prev)
         
         #Calcultaes the next value
-        x = x_prev + alpha(a)*(-1)*B.dot(grad)
+        x = x_prev + alpha(a)*(-1)*B.dot(grad.T)
         
         #Checks the the treshold
-        treshold = math.fabs(fun(x) - fun(x_prev)) < eps
+        #print(np.linalg.norm(grad))
+        treshold = np.linalg.norm(grad) < eps
     
     return [x, fun(x)]
-#End of run_gradient_descent
+#end of run_gradient_descent
+
 
 #----------------------------------------------------------------------
 #------------------------ Experiment Start ----------------------------
 #----------------------------------------------------------------------
+n = 500
+m = 200
+c = np.random.rand(1,n)
+#Puts each a_j as a column of the following matrix
+A = np.random.rand(n,m)
+global_eps = 0.01
 
 #Declares the global function and its gradient
 def main_function(x):
-    n = 500
-    m = 200
-    c = np.random.rand(1,n)
-    #Puts each a_j as a column of the following matrix
-    A = np.random.rand(n,m)
-    
+
     first_term = c.dot(x)
     second_term = (-1)*sum(map(lambda a: math.log(1 - a.dot(x)), A.T))
     third_term = (-1)*sum(map(lambda y: math.log(1 - y**2), x))
     
-    return first_term + second_term + third_term
-#-------------------
-#-----Constant------
-#-------------------
+    return(first_term + second_term + third_term)
+#end of main_function   
+
+def main_gradient(x):
+    first_term = np.array(c)
+    #print(first_term)
+    second_term = sum(map(lambda a: 1/(1 - a.dot(x)), A.T))*np.array(map(sum, A))
+    #print(second_term)
+    third_term = (-1)*np.array(map(lambda y: -2*y/(1 - y**2), x))
+    #print(third_term)
+    
+    
+    
+    return(first_term + second_term + third_term)
+#end of main_function
+
+#--------------------------
+#-----Gradient Descent-----
+#--------------------------
 
 
+#---- Constant ------
+#Runs the constant example
+def run_constant():
+    alpha_const = 1
+    B_const = np.identity(n)
+    alpha_fun = lambda a: alpha_const
+    B_matrix_fun = lambda b: B_const
+    
+    result = run_gradient_descent(dim = n,
+                                  fun = main_function,
+                                  gradient = main_gradient, 
+                                  alpha = alpha_fun, 
+                                  B_matrix = B_matrix_fun, 
+                                  eps = global_eps )
+    
+    return result
+#end of run_constant
+
+print run_constant()
+
+#print(run_gradient_descent(dim = 1,
+#                          fun = lambda x: (x-5)**2,
+#                          gradient = lambda x: 2*(x-5), 
+#                          alpha = lambda a:0.001, 
+#                          B_matrix = lambda b: np.matrix([[1]]), 
+#                          eps = 0.001 ))    
